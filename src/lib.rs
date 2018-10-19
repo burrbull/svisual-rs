ee8f72426ec207a83e468b509f79cb04fd0c8ba9
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2018-10-19 21:00:02 +0300

    v 0.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 #![no_std]
 
+use core::i32;
+
+use heapless::FnvIndexMap;
+
 pub const NAME_SZ : usize = 24;
 pub const PACKET_SZ : usize = 10;
 pub const VL_SZ : usize = (NAME_SZ+4+PACKET_SZ*4);
@@ -15,7 +19,7 @@ pub struct ValueRec {
     pub is_active     : bool,
     pub is_only_front : bool,
     pub vtype : ValueType,
-    pub vals : heapless::Vec<i32, heapless::consts::U10>,
+    pub vals : [i32;PACKET_SZ],
 }
 
 impl ValueRec {
@@ -24,76 +28,96 @@ impl ValueRec {
             is_active: false,
             is_only_front: false,
             vtype,
-            vals: heapless::Vec::new(),
+            vals: [0i32;PACKET_SZ]
         }
     }
 }
 
 pub enum AddError {
     MapOverflow,
-    PackageOverflow,
     BadName,
 }
 
-use core::i32;
+pub struct SVstruct<M> {
+    current: usize,
+    pub map: M
+}
+
+pub type SV<U> = SVstruct<FnvIndexMap<&'static [u8], ValueRec, U>>;
 
 pub trait AddValue {
     fn add_bool_value (&mut self, name: &'static [u8], value_in: bool, only_pos_front: bool
-    ) -> Result<(), AddError> {
+    ) -> Result<bool, AddError> {
         Ok(self.add_value(name, ValueType::Bool, i32::from(value_in), only_pos_front)?)
     }
     
     fn add_int_value (&mut self, name: &'static [u8], value_in: i32
-    ) -> Result<(), AddError> {
+    ) -> Result<bool, AddError> {
         Ok(self.add_value(name, ValueType::Int, value_in, false)?)
     }
     
     fn add_float_value (&mut self, name: &'static [u8], value_in: f32
-    ) -> Result<(), AddError> {
+    ) -> Result<bool, AddError> {
         Ok(self.add_value(name, ValueType::Float, value_in.to_bits() as i32, false)?)
     }
     
     fn add_value (&mut self, name: &'static [u8], vtype: ValueType, val: i32, only_pos_front: bool
-    ) -> Result<(), AddError>;
-    
-    fn clear_data (&mut self);
+    ) -> Result<bool, AddError>;
 }
 
 macro_rules! impl_add_value {
     ($U:ty) => {
-        impl AddValue for heapless::FnvIndexMap<&[u8], ValueRec, $U> {
+
+        impl SV<$U> {
+            pub fn new() -> Self {
+                Self {
+                    current: 0,
+                    map: FnvIndexMap::new()
+                }
+            }
+            
+            pub fn is_full(&self) -> bool {
+                self.current == PACKET_SZ
+            }
+            
+            pub fn next(&mut self) {
+                let previous = self.current;
+                self.current += 1;
+                if self.current > PACKET_SZ {
+                    self.current -= PACKET_SZ;
+                }
+                for (_, v) in self.map.iter_mut() {
+                    v.vals[self.current] = v.vals[previous]
+                }
+            }
+        }
+
+        impl AddValue for SV<$U> {
             fn add_value(
                 &mut self,
                 name: &'static [u8],
                 vtype: ValueType,
                 val: i32,
                 only_pos_front: bool
-            ) -> Result<(), AddError> {
-                if !self.contains_key(name) {
+            ) -> Result<bool, AddError> {
+                if !self.map.contains_key(name) {
                     let len = name.len();
                     if (len == 0) || (len >= NAME_SZ) ||
                     (name == b"=end=") || (name == b"=begin=") {
                         return Err(AddError::BadName);
                     }
-                    if self.insert(name, ValueRec::new(vtype)).is_err() {
+                    if self.map.insert(name, ValueRec::new(vtype)).is_err() {
                         return Err(AddError::MapOverflow);
                     }
                 }
                 
-                let vr = self.get_mut(name).unwrap();
-                if vr.vals.push(val).is_err() {
-                    return Err(AddError::PackageOverflow);
-                }
+                let vr = self.map.get_mut(name).unwrap();
+                vr.vals[self.current] = val;
                 vr.is_active = true;
+                //vr.vals[self.current] = val; // if interrupt breaks assignment
                 vr.is_only_front = only_pos_front;
                 
-                Ok(())
-            }
-            
-            fn clear_data (&mut self) {
-                for (_, v) in self {
-                    v.vals.clear()
-                }
+                Ok(self.is_full())
             }
         }
     };
