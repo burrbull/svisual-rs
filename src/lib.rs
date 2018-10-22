ba15dc31659cdaf9e04bcb4662c5dfe17bd9f4fb
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2018-10-22 10:48:35 +0300

    generic

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 use core::i32;
 
 use heapless::LinearMap;
+use generic_array::GenericArray;
+use typenum::marker_traits::PowerOfTwo;
+use generic_array::sequence::GenericSequence;
 
 pub const NAME_SZ : usize = 24;
 pub const PACKET_SZ : usize = 10;
@@ -15,20 +18,20 @@ pub enum ValueType {
     Float = 2,
 }
 
-pub struct ValueRec {
+pub struct ValueRec<P> where P: generic_array::ArrayLength<i32> {
     pub is_active     : bool,
     pub is_only_front : bool,
     pub vtype : ValueType,
-    pub vals : [i32;PACKET_SZ],
+    pub vals : GenericArray<i32, P>,
 }
 
-impl ValueRec {
-    pub fn new(vtype: ValueType) -> ValueRec {
+impl<P> ValueRec<P>  where P: generic_array::ArrayLength<i32> {
+    pub fn new(vtype: ValueType) -> ValueRec<P> {
         ValueRec {
             is_active: false,
             is_only_front: false,
             vtype,
-            vals: [0i32;PACKET_SZ]
+            vals: GenericArray::generate(|_| 0i32)
         }
     }
 }
@@ -43,7 +46,7 @@ pub struct SVstruct<M> {
     pub map: M
 }
 
-pub type SV<U> = SVstruct<LinearMap<&'static [u8], ValueRec, U>>;
+pub type SV<N, P> = SVstruct<LinearMap<&'static [u8], ValueRec<P>, N>>;
 
 pub trait AddValue {
     fn add_bool_value (&mut self, name: &'static [u8], value_in: bool, only_pos_front: bool
@@ -65,69 +68,62 @@ pub trait AddValue {
     ) -> Result<(), AddError>;
 }
 
-macro_rules! impl_add_value {
-    ($U:ty) => {
-
-        impl SV<$U> {
-            pub fn new() -> Self {
-                Self {
-                    current: 0,
-                    map: LinearMap::new()
-                }
-            }
-            
-            pub fn next<F>(&mut self, f: F)
-            where
-                F: FnOnce(&Self) {
-                let previous = self.current;
-                self.current += 1;
-                if self.current >= PACKET_SZ {
-                    self.current -= PACKET_SZ;
-                    f(self);
-                }
-                for (_, v) in self.map.iter_mut() {
-                    v.vals[self.current] = v.vals[previous]
-                }
-            }
+impl<N, P> SV<N, P>
+where
+N: heapless::ArrayLength<(&'static [u8], ValueRec<P>)> + PowerOfTwo,
+P: generic_array::ArrayLength<i32>
+{
+    pub fn new() -> Self {
+        Self {
+            current: 0,
+            map: LinearMap::new()
         }
-
-        impl AddValue for SV<$U> {
-            fn add_value(
-                &mut self,
-                name: &'static [u8],
-                vtype: ValueType,
-                val: i32,
-                only_pos_front: bool
-            ) -> Result<(), AddError> {
-                if !self.map.contains_key(&name) {
-                    let len = name.len();
-                    if (len == 0) || (len >= NAME_SZ) ||
-                    (name == b"=end=") || (name == b"=begin=") {
-                        return Err(AddError::BadName);
-                    }
-                    if self.map.insert(name, ValueRec::new(vtype)).is_err() {
-                        return Err(AddError::MapOverflow);
-                    }
-                }
-                
-                let vr = self.map.get_mut(name).unwrap();
-                vr.vals[self.current] = val;
-                vr.is_active = true;
-                //vr.vals[self.current] = val; // if interrupt breaks assignment
-                vr.is_only_front = only_pos_front;
-                
-                Ok(())
-            }
+    }
+    
+    pub fn next<F>(&mut self, f: F)
+    where
+        F: FnOnce(&Self) {
+        let previous = self.current;
+        self.current += 1;
+        if self.current >= PACKET_SZ {
+            self.current -= PACKET_SZ;
+            f(self);
         }
-    };
+        for (_, v) in self.map.iter_mut() {
+            v.vals[self.current] = v.vals[previous]
+        }
+    }
 }
 
-use heapless::consts::{U1, U2, U4, U8, U16, U32, U64, U128};
-impl_add_value!(U1);
-impl_add_value!(U2);
-impl_add_value!(U4);
-impl_add_value!(U8);
-impl_add_value!(U16);
-impl_add_value!(U32);
-impl_add_value!(U64);
-impl_add_value!(U128);
+impl<N, P> AddValue for SV<N, P>
+where
+N: heapless::ArrayLength<(&'static [u8], ValueRec<P>)> + PowerOfTwo,
+P: generic_array::ArrayLength<i32>
+{
+    fn add_value(
+        &mut self,
+        name: &'static [u8],
+        vtype: ValueType,
+        val: i32,
+        only_pos_front: bool
+    ) -> Result<(), AddError> {
+        if !self.map.contains_key(&name) {
+            let len = name.len();
+            if (len == 0) || (len >= NAME_SZ) ||
+            (name == b"=end=") || (name == b"=begin=") {
+                return Err(AddError::BadName);
+            }
+            if self.map.insert(name, ValueRec::new(vtype)).is_err() {
+                return Err(AddError::MapOverflow);
+            }
+        }
+        
+        let vr = self.map.get_mut(name).unwrap();
+        vr.vals[self.current] = val;
+        vr.is_active = true;
+        //vr.vals[self.current] = val; // if interrupt breaks assignment
+        vr.is_only_front = only_pos_front;
+        
+        Ok(())
+    }
+}
