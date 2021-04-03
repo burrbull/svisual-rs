5c94e1b8c4ad56f9d5c9d0f4a0f0fd11f10094bd
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2021-04-03 07:26:54 +0300

    use const generics

--- src/lib.rs
+++ src/lib.rs
@@ -3,34 +3,31 @@
 use core::i32;
 
 use heapless::LinearMap;
-use generic_array::GenericArray;
-use generic_array::sequence::GenericSequence;
-use typenum::marker_traits::PowerOfTwo;
 
-pub const NAME_SZ : usize = 24;
+pub const NAME_SZ: usize = 24;
 
-#[derive(Clone,Copy,Debug,PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum ValueType {
-    Bool  = 0,
-    Int   = 1,
+    Bool = 0,
+    Int = 1,
     Float = 2,
 }
 
 #[derive(Clone)]
-pub struct ValueRec<P> where P: generic_array::ArrayLength<i32> {
-    pub is_active     : bool,
-    pub is_only_front : bool,
-    pub vtype : ValueType,
-    pub vals : GenericArray<i32, P>,
+pub struct ValueRec<const P: usize> {
+    pub is_active: bool,
+    pub is_only_front: bool,
+    pub vtype: ValueType,
+    pub vals: [i32; P],
 }
 
-impl<P> ValueRec<P> where P: generic_array::ArrayLength<i32> {
-    pub fn new(vtype: ValueType) -> ValueRec<P> {
-        ValueRec {
+impl<const P: usize> ValueRec<P> {
+    pub const fn new(vtype: ValueType) -> Self {
+        Self {
             is_active: false,
             is_only_front: false,
             vtype,
-            vals: GenericArray::generate(|_| {0i32})
+            vals: [0; P],
         }
     }
 }
@@ -39,54 +36,57 @@ pub enum AddError {
     MapOverflow,
 }
 
-#[derive(Clone,Copy)]
+#[derive(Clone, Copy)]
 pub struct SVstruct<M> {
     current: usize,
-    pub map: M
+    pub map: M,
 }
 
-pub type SV<N, P> = SVstruct<LinearMap<&'static [u8], ValueRec<P>, N>>;
+pub type SV<const N: usize, const P: usize> = SVstruct<LinearMap<&'static [u8], ValueRec<P>, N>>;
 
 pub trait AddValue {
-    fn add_bool_value (&mut self, name: &'static [u8], value_in: bool, only_pos_front: bool
+    fn add_bool_value(
+        &mut self,
+        name: &'static [u8],
+        value_in: bool,
+        only_pos_front: bool,
     ) -> Result<(), AddError> {
         Ok(self.add_value(name, ValueType::Bool, i32::from(value_in), only_pos_front)?)
     }
-    
-    fn add_int_value (&mut self, name: &'static [u8], value_in: i32
-    ) -> Result<(), AddError> {
+
+    fn add_int_value(&mut self, name: &'static [u8], value_in: i32) -> Result<(), AddError> {
         Ok(self.add_value(name, ValueType::Int, value_in, false)?)
     }
-    
-    fn add_float_value (&mut self, name: &'static [u8], value_in: f32
-    ) -> Result<(), AddError> {
+
+    fn add_float_value(&mut self, name: &'static [u8], value_in: f32) -> Result<(), AddError> {
         Ok(self.add_value(name, ValueType::Float, value_in.to_bits() as i32, false)?)
     }
-    
-    fn add_value (&mut self, name: &'static [u8], vtype: ValueType, val: i32, only_pos_front: bool
+
+    fn add_value(
+        &mut self,
+        name: &'static [u8],
+        vtype: ValueType,
+        val: i32,
+        only_pos_front: bool,
     ) -> Result<(), AddError>;
 }
 
-impl<N, P> SV<N, P>
-where
-    N: heapless::ArrayLength<(&'static [u8], ValueRec<P>)> + PowerOfTwo,
-    P: generic_array::ArrayLength<i32> + typenum::marker_traits::Unsigned
-{
-    pub fn new() -> Self {
+impl<const N: usize, const P: usize> SV<N, P> {
+    pub const fn new() -> Self {
         Self {
             current: 0,
-            map: LinearMap::new()
+            map: LinearMap::new(),
         }
     }
-    
+
     pub fn next<F>(&mut self, f: F)
     where
-        F: FnOnce(&Self) {
+        F: FnOnce(&Self),
+    {
         let previous = self.current;
         self.current += 1;
-        let packet_size = P::to_usize();
-        if self.current >= packet_size {
-            self.current -= packet_size;
+        if self.current >= P {
+            self.current -= P;
             f(self);
         }
         for (_, v) in self.map.iter_mut() {
@@ -95,17 +95,13 @@ where
     }
 }
 
-impl<N, P> AddValue for SV<N, P>
-where
-    N: heapless::ArrayLength<(&'static [u8], ValueRec<P>)>,
-    P: generic_array::ArrayLength<i32>
-{
+impl<const N: usize, const P: usize> AddValue for SV<N, P> {
     fn add_value(
         &mut self,
         name: &'static [u8],
         vtype: ValueType,
         val: i32,
-        only_pos_front: bool
+        only_pos_front: bool,
     ) -> Result<(), AddError> {
         if !self.map.contains_key(&name) {
             debug_assert!((name.len() > 0) && (name.len() <= NAME_SZ));
@@ -114,12 +110,12 @@ where
                 return Err(AddError::MapOverflow);
             }
         }
-        
+
         let vr = self.map.get_mut(name).unwrap();
         vr.vals[self.current] = val;
         vr.is_active = true;
         vr.is_only_front = only_pos_front;
-        
+
         Ok(())
     }
 }
